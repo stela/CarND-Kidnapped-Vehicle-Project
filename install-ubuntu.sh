a299da163dad097b1c3b20568bb0b43c8019e8d1
Stefan Larsson <stefan@larsson.nu>
2018-03-09 23:17:57 +0100

    Get the proper libuv1

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -xe
-sudo apt-get install -y libuv-dev libssl-dev
-git clone https://github.com/uWebSockets/uWebSockets 
+# On Ubuntu trusty, libuv1-dev isn't available, and libuv-dev refers to the wrong release, 0.10
+# Need to get libuv1_1.8.0-1_amd64.deb and libuv1-dev_1.8.0-1_amd64.deb from elsewhere (included in xenial 16.04LTS)
+sudo apt-get install -y libssl-dev
+sudo apt install http://mirrors.kernel.org/ubuntu/pool/universe/libu/libuv1/libuv1_1.8.0-1_amd64.deb
+sudo apt install http://mirrors.kernel.org/ubuntu/pool/universe/libu/libuv1/libuv1-dev_1.8.0-1_amd64.deb
+sudo apt install http://mirrors.kernel.org/ubuntu/pool/universe/libu/libuv1/libuv1-dbg_1.8.0-1_amd64.deb
+git clone https://github.com/uWebSockets/uWebSockets
 cd uWebSockets
 git checkout e94b6e1
 mkdir build
